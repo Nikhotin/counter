448aa2cac6de9ef522080c0743cbacad67795f09
Roman <r.nikhotin@gmail.com>
2020-03-10 16:34:28 +0300

    Добавил middleware аутентификации для нескольких маршрутов

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,27 @@
 const users = require('../app/controllers/users');
 const counter = require('../app/controllers/counter');
+const auth = require('../app/controllers/auth');
+const authMiddleware = require('../app/middleware/auth');
 
 module.exports = (app) => {
   // main page
   app.get('/', (req, res) => res.render('index'));
 
   // users
-  app.get('/users', users.getAll);
-  app.post('/users', users.create);
-  app.put('/users/:userId', users.update);
-  app.delete('/users/:userId', users.remove);
+  app.get('/users', authMiddleware, users.getAll);
+  app.post('/users', authMiddleware, users.create);
+  app.put('/users/:userId', authMiddleware, users.update);
+  app.delete('/users/:userId', authMiddleware, users.remove);
   app.get('/profile/:userId', (req, res) => res.render('profile'));
 
   // counter
   app.get('/counter', counter.getEveryCount);
   app.get('/counter/:userId', counter.getEveryCountById);
-  app.post('/counter', counter.create);
+  app.post('/counter', authMiddleware, counter.create);
+
+  // auth
+  app.get('/registration', (req, res) => res.render('registry'));
+  app.get('/login', (req, res) => res.render('login'));
+  // app.get('/logout');
+  app.post('/singin', auth.singIn);
 };
