ab4ff26ef2f77b32355675f61d670cd245d190c7
Roman <r.nikhotin@gmail.com>
2020-03-10 16:31:19 +0300

    В конфиг routes добавил новые маршруты

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,19 @@
+const users = require('../app/controllers/users');
+const counter = require('../app/controllers/counter');
+
 module.exports = (app) => {
   // main page
   app.get('/', (req, res) => res.render('index'));
+
+  // users
+  app.get('/users', users.getAll);
+  app.post('/users', users.create);
+  app.put('/users/:userId', users.update);
+  app.delete('/users/:userId', users.remove);
+  app.get('/profile/:userId', (req, res) => res.render('profile'));
+
+  // counter
+  app.get('/counter', counter.getEveryCount);
+  app.get('/counter/:userId', counter.getEveryCountById);
+  app.post('/counter', counter.create);
 };
